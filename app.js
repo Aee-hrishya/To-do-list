b24836030ea317df6e375940cf46eb71ff95ae1e
hrishikesh <hshinde166@gmail.com>
2021-12-11 01:40:17 +0530

    Persisted add tasks on local storage

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ loadEventListener();
 //function loadEventListener
 function loadEventListener() {
 
+    //DOM content loaded
+    document.addEventListener("DOMContentLoaded", getTasks);
+
     //Add task event
     form.addEventListener("submit", addTask);
 
@@ -21,8 +24,39 @@ function loadEventListener() {
     clearBtn.addEventListener("click", clearTasks);
 
     //Filter tasks
-    filterTasks.addEventListener("keyup",filterTask);
+    filterTasks.addEventListener("keyup", filterTask);
 }
+
+//Get tasks from local storage
+function getTasks() {
+    let tasks;
+    if (localStorage.getItem("tasks") === null) {
+        tasks = [];
+    }
+    else {
+        tasks = JSON.parse(localStorage.getItem("tasks"));
+    }
+
+    tasks.forEach(function (task) {
+        //create li element
+        const li = document.createElement("li");
+        //add class
+        li.className = "collection-item";
+        //create text node and append to li
+        li.appendChild(document.createTextNode(task));
+        //create a link 
+        const link = document.createElement("a");
+        //add class
+        link.className = "delete-item secondary-content";
+        //add icon in html
+        link.innerHTML = '<i class="far fa-trash-alt"></i>';
+        //append link to li
+        li.appendChild(link);
+        //apppend li to ul
+        taskList.appendChild(li);
+    });
+}
+
 
 //addTask function
 function addTask(e) {
@@ -33,25 +67,50 @@ function addTask(e) {
 
     //create li element
     const li = document.createElement("li");
+
     //add class
     li.className = "collection-item";
+
     //create text node and append to li
     li.appendChild(document.createTextNode(taskInput.value));
+
     //create a link 
     const link = document.createElement("a");
+
     //add class
     link.className = "delete-item secondary-content";
+
     //add icon in html
     link.innerHTML = '<i class="far fa-trash-alt"></i>';
+
     //append link to li
     li.appendChild(link);
+    
     //apppend li to ul
     taskList.appendChild(li);
+
+    //Store task in local storage
+    storeTaskInLocalStorage(taskInput.value);
 
     //clear input
     taskInput.value = "";
 
     e.preventDefault();
+}
+
+//store task
+function storeTaskInLocalStorage(task) {
+    let tasks;
+    if (localStorage.getItem("tasks") === null) {
+        tasks = [];
+    }
+    else {
+        tasks = JSON.parse(localStorage.getItem("tasks"));
+    }
+
+    tasks.push(task);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+
 }
 
 //removeTask function
@@ -64,26 +123,26 @@ function removeTask(e) {
 }
 
 //clearTasks function
-function clearTasks(e){
+function clearTasks(e) {
     //taskList.innerHTML = "";
 
     //Faster way to do the same thing is using a while loop(We can even use the above method doesnt matter much)
-    while(taskList.firstChild){
+    while (taskList.firstChild) {
         taskList.removeChild(taskList.firstChild);
     }
 }
 
 //filterTasks function
-function filterTask(e){
+function filterTask(e) {
     const text = e.target.value.toLowerCase();//Converting all values to lowercase
-    
+
     //We can use forEach on the ".collection-item " as it returns the nodelist which we can iterate through
-    document.querySelectorAll(".collection-item").forEach(function(task){
-        const item = task.firstChild.textContent;
-        if(item.toLowerCase().indexOf(text) != -1){
+    document.querySelectorAll(".collection-item").forEach(function (task) {
+        const item = task.firstChild.textContent; //We get the text content by iterating 
+        if (item.toLowerCase().indexOf(text) != -1) {
             task.style.display = "block";
         }
-        else{
+        else {
             task.style.display = "none";
         }
     });
